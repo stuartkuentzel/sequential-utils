16f54967eefed1b370b3c68b46f4c099f7f16188
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-01-26 16:01:19 +0900

    adds midi unpacking

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,13 @@
-import { isNumberParseable } from './';
+import { unpackageMidi } from './';
 
-describe('unit | isNumberParseable', () => {
-  it('returns `true` for values parseable number', () => {
-    expect(isNumberParseable('-7.5')).toBe(true);
-    expect(isNumberParseable(false)).toBe(true);
-    expect(isNumberParseable(1892)).toBe(true);
-  });
+describe('parse patch name from midi data', () => {
+  // eslint-disable-next-line prettier/prettier
+  const data = [240,1,40,96,0,36,25,0,16,73,6,0,3,36,19,5,0,32,80,6,2,0,0,0,1,1,127,60,20,0,48,39,4,124,1,0,112,0,28,0,0,120,127,33,64,0,0,64,1,0,80,124,0,1,0,0,0,0,0,0,113,72,0,0,0,36,3,7,0,2,127,0,0,0,0,0,0,0,127,0,0,0,0,0,41,0,63,0,124,1,96,15,40,0,127,0,120,3,64,31,10,0,126,0,112,7,0,0,0,0,0,0,24,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,82,38,26,104,10,14,47,75,114,16,8,8,4,2,1,64,32,16,0,8,4,2,0,64,0,16,247]
+  const uint8Array = new Uint8Array(data.length);
+  uint8Array.set(data, 0);
 
-  it('returns `false` for values non parseable to number', () => {
-    expect(isNumberParseable('A8sa')).toBe(false);
-    expect(isNumberParseable({})).toBe(false);
-    expect(isNumberParseable(NaN)).toBe(false);
-    expect(isNumberParseable('18L')).toBe(false);
+  it('should contain the patch name', () => {
+    const unpackaged = unpackageMidi(uint8Array);
+    expect(String.fromCharCode(...unpackaged)).toContain('RL Shaker');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 /**
- * A Branded Type for values parseable to number.
+ * Converted from https://github.com/Chysn/sequential_lib/blob/main/sequential_packing.h#L84
+ * @param data
  */
-export type NumberParseable = (number | string | boolean) & {
-  readonly isNumberParseble: unique symbol;
-};
+export const unpackageMidi = (data: Uint8Array): Uint8Array => {
+  const SEQUENTIAL_DATA_MAX = 128000;
 
-/**
- * Check if value is parseable to number.
- * @example ```ts
- * isNumberParseable('AAAA');
- * //=> false
- *
- * isNumberParseable('100');
- * //=> true
- *
- * if (!isNumberParseable(value))
- *   throw new Error('Value can\'t be parseable to `Number`.')
- * return Number(value);
- * ```
- * @param value - An `unknown` value to be checked.
- */
-export const isNumberParseable = (value: unknown): value is NumberParseable =>
-  !Number.isNaN(Number(value));
+  const values: Uint8Array = new Uint8Array(data.length - 1);
+  let packbyte = 0;
+  let pos = 0;
+  let size = 0;
+  for (let i = 0; i < data.length; i++) {
+    let currentByte = data[i];
+
+    if (pos === 0) {
+      packbyte = currentByte;
+    } else {
+      if (packbyte & (1 << (pos - 1))) {
+        currentByte |= 0x80;
+      }
+      values[size++] = currentByte;
+    }
+
+    pos++;
+    pos &= 0x07;
+    if (size > SEQUENTIAL_DATA_MAX) {
+      break;
+    }
+  }
+  return values;
+};
